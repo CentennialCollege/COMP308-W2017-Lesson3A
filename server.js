f2742df04a93ca9523afb8c9ddb477bb31badeac
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-01-28 11:32:56 -0500

    updated port listening information

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ let express = require('express');
 let app = express();
 
 // create the port constant
-//const port = 3000;
+const localport = 3000;
+
+/**
+ * Get port from environment and store in Express.
+ */
+let port = process.env.PORT || localport;
+app.set('port', port);
 
 // start listening on the port
-app.listen(() => {
-  console.log(`Server started...`);
-})
-//console.log(`Server started at http://localhost:${port}`);
+app.listen(port);
+console.log(`Server started at http://localhost:${port}`);
 
 // ROUTING - mounted our routes
 
